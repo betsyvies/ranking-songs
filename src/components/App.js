355a6dceae02ebd843633ec861999da93dbf05de
Danielalab <olabolacrayola@hotmail.com>
2018-03-10 12:32:36 -0500

    añadiendo data de canciones

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './global/App.css';
 
+const songs = [
+  {id: 1, name: 'Sign of the time', img: 'assets/images/1.jpg', vote: '15'},
+  {id: 2, name: 'Havana', img: 'assets/images/2.jpg', vote: '14' },
+  {id: 3, name: 'Eye of the tiger', img: 'assets/images/3.png', vote: '12'},
+  {id: 4, name: 'Thats what i like', img: 'assets/images/4.jpg', vote: '11'},
+  {id: 5, name: 'Perfect', img: 'assets/images/5.jpg', vote: '13'},
+  {id: 6, name: 'Love me like you do', img: 'assets/images/6.jpg', vote: '10'},  
+];
+
 const App = () => {
   render() {
     return (
       <div className="App">
-      </div>
+      </div>  
     );
   }
 }
