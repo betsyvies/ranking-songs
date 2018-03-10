a2ae611359a1302664f296efa9a2058767b0eb9e
betsyvies <ing.betsyvies@gmail.com>
2018-03-10 13:11:44 -0500

    añadiendo estructura

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,53 @@ import React from 'react';
 import './global/App.css';
 
 const App = () => (
-  <div className="App">
-  </div>
+  <div>
+    <div>
+      <img src="previous.png" alt=""/>
+      <img src="" alt=""/>
+      <img src="next.png" alt=""/>
+    </div>
+    <div>
+      <ul>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+        <li>
+          <span>name</span>
+          <img src="assets/icons/plus.png" alt="plus"/>
+          <img src="assets/icons/minus.png" alt="minus"/>
+          <span>23</span>
+        </li>
+      </ul>
+    </div>
+  </div>  
 )
 
 
