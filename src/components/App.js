c205db3a6f52e76199f50ee622cb9e9743dc8d62
betsyvies <ing.betsyvies@gmail.com>
2018-03-10 12:35:37 -0500

    aceptando cambios

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './global/App.css';
 
-const App = () => {
-  render() {
-    return (
-      <div className="App">
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+  </div>
+)
+
 
 export default App;
