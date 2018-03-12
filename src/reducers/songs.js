b3c60562629f9b14272a9697ef6e5cfc593e588f
betsyvies <ing.betsyvies@gmail.com>
2018-03-11 23:08:02 -0500

    añadiendo containers y reducers

--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import AllSongs from './AllSongs';
-
-const songs = [
+const initialSongs = [
   {id: 1, name: 'Sign of the time', img: 'assets/images/1.jpg', vote: '10'},
   {id: 2, name: 'Havana', img: 'assets/images/2.jpg', vote: '15' },
   {id: 3, name: 'Eye of the tiger', img: 'assets/images/3.png', vote: '11'},
@@ -10,33 +7,18 @@ const songs = [
   {id: 6, name: 'Love me like you do', img: 'assets/images/6.jpg', vote: '14'},  
 ];
 
-const songsOrder = songs.sort(function (a, b) {
-  console.log(a.vote);
+const SongsOrder = initialSongs.sort(function (a, b) {
   if (a.vote > b.vote) {
     return 1;
   }
   if (a.vote < b.vote) {
     return -1;
   }
-  // a must be equal to b
   return 0;
-});
+}).reverse();
 
-const SongsList = () => (
-  <div>
-    <ul>{songsOrder.reverse().map(song =>
-      <AllSongs key={song.id}
-        {...song}
-      />
-    )}
-  </ul>
-</div>
-);
+const songs = (state = SongsOrder, action) => {
+  return state
+}
 
-
-
-
-
-
-
-export default SongsList;
+export default songs
