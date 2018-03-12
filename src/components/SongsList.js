197ccf86d4a9e6c1417044cf292950512023b389
betsyvies <ing.betsyvies@gmail.com>
2018-03-11 21:28:21 -0500

    ordenando votes

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -2,17 +2,29 @@ import React from 'react';
 import AllSongs from './AllSongs';
 
 const songs = [
-  {id: 1, name: 'Sign of the time', img: 'assets/images/1.jpg', vote: '15'},
-  {id: 2, name: 'Havana', img: 'assets/images/2.jpg', vote: '14' },
-  {id: 3, name: 'Eye of the tiger', img: 'assets/images/3.png', vote: '12'},
-  {id: 4, name: 'Thats what i like', img: 'assets/images/4.jpg', vote: '11'},
-  {id: 5, name: 'Perfect', img: 'assets/images/5.jpg', vote: '13'},
-  {id: 6, name: 'Love me like you do', img: 'assets/images/6.jpg', vote: '10'},  
+  {id: 1, name: 'Sign of the time', img: 'assets/images/1.jpg', vote: '10'},
+  {id: 2, name: 'Havana', img: 'assets/images/2.jpg', vote: '15' },
+  {id: 3, name: 'Eye of the tiger', img: 'assets/images/3.png', vote: '11'},
+  {id: 4, name: 'Thats what i like', img: 'assets/images/4.jpg', vote: '13'},
+  {id: 5, name: 'Perfect', img: 'assets/images/5.jpg', vote: '12'},
+  {id: 6, name: 'Love me like you do', img: 'assets/images/6.jpg', vote: '14'},  
 ];
+
+const songsOrder = songs.sort(function (a, b) {
+  console.log(a.vote);
+  if (a.vote > b.vote) {
+    return 1;
+  }
+  if (a.vote < b.vote) {
+    return -1;
+  }
+  // a must be equal to b
+  return 0;
+});
 
 const SongsList = () => (
   <div>
-    <ul>{songs.map(song =>
+    <ul>{songsOrder.reverse().map(song =>
       <AllSongs key={song.id}
         {...song}
       />
@@ -20,5 +32,11 @@ const SongsList = () => (
   </ul>
 </div>
 );
+
+
+
+
+
+
 
 export default SongsList;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import  {render }from 'react-dom';
+import { render } from 'react-dom';
+// import { createStore } from 'redux';
+// import { Provider } from 'react-redux'
 import './components/global/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+// import reducer from './reducers'
+
+// const store = createStore(
+//   reducer,
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+// )
 
 render(
-  <App />, document.getElementById('root')
+   <App />, 
+  document.getElementById('root')
 );
 
 registerServiceWorker();
